e8c15af7cdb4c9d4fcb4be9ecb7cab53c5bc52b5
poider <moadammari20@gmail.com>
2023-09-27 09:41:38 +0100

    added generated_at and the rest on IMF

--- IMF/datamapper.js
+++ IMF/datamapper.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
+const { get } = require('express/lib/response');
 const fs = require('fs');
 const path = require('path')
-async function dataFetcher(indicator, ...args)
-{
-    if(!indicator)
+async function dataFetcher(indicator, ...args) {
+    if (!indicator)
         throw new Error('Indicator is required')
     let apiLink = 'https://www.imf.org/external/datamapper/api/v1'
 
@@ -16,99 +16,135 @@ async function dataFetcher(indicator, ...args)
     return response.data
 }
 
-function getAfricanCountries()
-{
+function getAfricanCountries() {
     const jsonPath = path.join(__dirname, 'input', 'african_countries.json')
     const countriesData = fs.readFileSync(jsonPath, 'utf-8')
     return JSON.parse(countriesData)
 }
 
-function getObjectKeys(object)
-{
+function getObjectKeys(object) {
     const keys = []
-    for (const key in object)
-    {
+    for (const key in object) {
         keys.push(key)
     }
     return keys
 }
 
-function isAfrican(code, countriesData)
-{
-    for (const countryData of countriesData)
-    {
+function isAfrican(code, countriesData) {
+    for (const countryData of countriesData) {
         if (countryData["code"] === code)
             return countryData["country"]
     }
     return null
 }
 
-function formatData(data, unit, label)
+function getCountriesRef() {
+    const jsonPath = path.join(__dirname, '..', 'Regions', 'African_regions.json');
+    const countriesRef = fs.readFileSync(jsonPath, 'utf-8');
+    const parsedCountriesRef = JSON.parse(countriesRef);
+
+    const countriesRegions = parsedCountriesRef.map(countryData => {
+        const { country, iso_code } = countryData;
+        return [country.toUpperCase() , iso_code];
+    });
+    return new Map(countriesRegions);
+
+}
+
+const imfCountriesChanged = new Map([
+    ["CONGO, REPUBLIC OF",'Congo (Brazzaville)'],
+    ["CONGO, DEM. REP. OF THE", 'Congo (Kinshasa)'],
+    ["SÃO TOMÉ AND PRÍNCIPE", 'Sao Tome and Principe'],
+    ["SOUTH SUDAN, REPUBLIC OF", "South Sudan"]
+])
+function getCountryDataToInsert(country, countriesRef)
 {
+    country = country.toUpperCase();
+    if(countriesRef.get(country))
+    {
+        const iso_code = countriesRef.get(country);
+        return {country, iso_code};
+    }
+    else
+    {
+        if(imfCountriesChanged.get(country))
+        {
+            const updatedCountry = imfCountriesChanged.get(country).toUpperCase();
+            const iso_code = countriesRef.get(updatedCountry);
+            // console.log(country, iso_code)
+            return {country : updatedCountry, iso_code};
+        }
+        else
+        return {country, iso_code : null};
+    }    
+}
+
+function formatData(data, unit, label) {
     const africanCountries = getAfricanCountries()
 
     const formattedData = []
     const values = data["values"]
+    const countriesRef = getCountriesRef()
     const indicator = getObjectKeys(values)[0]
     getObjectKeys(values[indicator]).forEach((countryCode) => {
         const country = isAfrican(countryCode, africanCountries)
-        if (country)
-        {
-            for(const year in values[indicator][countryCode])
-            {
-                if (year < 2000)
-                    continue
-                const value = values[indicator][countryCode][year]
-                formattedData.push({
-                    "indicator": label,
-                    "country": country,
-                    "country code": countryCode,
-                    "year": year,
-                    "value": value,
-                    "unit": unit? unit : null
-                    // "indicator": indicator
-                })
-            }
+        if (country) {
+            const countryToInsert = getCountryDataToInsert(country,countriesRef)
+            
+                for (const year in values[indicator][countryCode]) {
+                    if (year < 2000)
+                        continue
+                    const value = values[indicator][countryCode][year]
+                    formattedData.push({
+                        "indicator": label,
+                        "country": countryToInsert.country ? countryToInsert.country : null,
+                        "country code": countryToInsert.iso_code ? countryToInsert.iso_code : null,
+                        "year": year,
+                        "value": value,
+                        "unit": unit ? unit : null,
+                        "source": "IMF",
+                        "api_url" : "https://www.imf.org/external/datamapper/api/v1",
+                        "extracted_on": new Date().toISOString().slice(0, 10)
+
+                        // "indicator": indicator
+                    })
+                }
+            
         }
     })
     return formattedData
 }
 
 
-async function dataMapper(indicator, fileName, unit, label)
-{
+async function dataMapper(indicator, fileName, unit, label) {
     const fetchedData = await dataFetcher(indicator)
     const formattedData = formatData(fetchedData, unit, label)
     stringified = JSON.stringify(formattedData, null, 2)
-    
+
     fs.writeFileSync(path.join(__dirname, 'output', fileName), stringified)
 }
 
 
-async function getFilesReady(indicator, fileName)
-{
+async function getFilesReady(indicator, fileName) {
     const indicatorInfos = await dataFetcher('indicators')
-    
+
     const unit = []
     const labels = []
-    for(let i = 0; i < indicator.length; i++)
-    {
+    for (let i = 0; i < indicator.length; i++) {
         unit.push(indicatorInfos["indicators"][indicator[i]]["unit"])
         labels.push(indicatorInfos["indicators"][indicator[i]]["label"])
     }
 
-    for (let i = 0; i < indicator.length; i++)
-    {
+    for (let i = 0; i < indicator.length; i++) {
         await dataMapper(indicator[i], fileName[i], unit[i], labels[i])
     }
 }
 
-async function main()
-{
-    const indicators = ['d','BRASS_MI']
+async function main() {
+    const indicators = ['d', 'BRASS_MI']
     const fileNames = ["Gross public debt, percent of GDP.json", "Reserves (Months of Imports).json"]
     await getFilesReady(indicators, fileNames)
-    
+
 }
 
 main()
---
+++ Regions/uppercasing.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+const fs = require('fs');
+[
+    {
+      "country": "Algeria",
+      "iso_code": "DZ",
+      "subregion": "Northern Africa"
+    }
+]
+
+function UppercasingJson()
+{
+    const jsonPath = path.join(__dirname, 'African_regions.json');
+    const countriesRef = fs.readFileSync(jsonPath, 'utf-8');
+    const parsedCountriesRef = JSON.parse(countriesRef);
+    const countriesRegions = parsedCountriesRef.map(countryData => {
+        const { country, iso_code, subregion } = countryData;
+        return {country : country.toUpperCase(), iso_code,subregion: subregion.toUpperCase()};
+    });
+    return countriesRegions;
+}
+
+const countriesRegions = UppercasingJson();
+const jsonPath = path.join(__dirname, 'African_regions.json');  
+fs.writeFileSync(jsonPath, JSON.stringify(countriesRegions, null, 2));
+
+
